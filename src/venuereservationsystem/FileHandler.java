4158c526b4889fbc8e69d6d3c3c7b4648f534dab
Hikari9Riki <kanekihimito29@gmail.com>
2025-05-24 12:33:06 +0800

    commit

--- src/venuereservationsystem/FileHandler.java
+++ src/venuereservationsystem/FileHandler.java
@@ -4,15 +4,20 @@
  */
 package venuereservationsystem;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.List;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author kanek
  */
 public class FileHandler {
+
+    
     
     public void initializeFiles() {
         createFile("users.txt");
@@ -46,4 +51,28 @@ public class FileHandler {
             e.printStackTrace();
         }
     }
+    
+    public ArrayList<User> readUser() throws FileNotFoundException{
+        ArrayList<User> users = new ArrayList();
+        File file = new File("users.txt");
+        Scanner read = new Scanner(file);
+        
+        while(read.hasNext()){
+            String line = read.nextLine();
+            String[] parts = line.split(",");
+            String role = parts[0].trim();
+            String userID = parts[1].trim();
+            String name = parts[2].trim();
+            String email = parts[3].trim();
+            String phone = parts[4].trim();
+            String password = parts[5].trim();
+            
+            User user = new User(name, email, phone,  password, role, userID);
+            users.add(user);
+                    
+        }
+        return users;
+    }
+
+   
 }
